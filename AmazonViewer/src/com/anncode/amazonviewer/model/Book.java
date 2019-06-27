410ea3c7e048f6a0562812119282a22a76c19748
sigmotoa <sergio@newingenieros.com>
2019-06-26 22:25:52 -0500

    Declaracion clase page

--- AmazonViewer/src/com/anncode/amazonviewer/model/Book.java
+++ AmazonViewer/src/com/anncode/amazonviewer/model/Book.java
@@ -122,4 +122,37 @@ public class Book extends Publication implements IVisualizable {
 		return books;
 	}
 	
+	public static class Page{
+		private int id;
+		private int number;
+		private String content;
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public int getNumber() {
+			return number;
+		}
+		public void setNumber(int number) {
+			this.number = number;
+		}
+		public String getContent() {
+			return content;
+		}
+		public void setContent(String content) {
+			this.content = content;
+		}
+		public Page(int number, String content) {
+			super();
+			this.number = number;
+			this.content = content;
+		}
+		
+		
+		
+		
+	}
+	
 }
