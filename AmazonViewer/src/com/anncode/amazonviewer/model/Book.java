a72f93d29ecdc2d0e36683d2ac2180f1966cc010
sigmotoa <sergio@newingenieros.com>
2019-06-27 15:03:19 -0500

    Se realiza lectura de Books, a traves de clase interna Page

--- AmazonViewer/src/com/anncode/amazonviewer/model/Book.java
+++ AmazonViewer/src/com/anncode/amazonviewer/model/Book.java
@@ -3,79 +3,73 @@ package com.anncode.amazonviewer.model;
 import java.util.ArrayList;
 import java.util.Date;
 
+import com.anncode.util.AmazonUtil;
+
+import jdk.nashorn.api.tree.ForInLoopTree;
+
 public class Book extends Publication implements IVisualizable {
 	private int id;
 	private String isbn;
 	private boolean readed;
 	private int timeReaded;
-	
-	
-	public Book(String title, Date edititionDate, String editorial, String[] authors) {
+	private ArrayList<Page> pages;
+
+	public Book(String title, Date edititionDate, String editorial, String[] authors, ArrayList<Page> pages) {
 		super(title, edititionDate, editorial);
 		// TODO Auto-generated constructor stub
 		setAuthors(authors);
+		this.pages=pages;
 	}
-
 
 	public int getId() {
 		return id;
 	}
 
-
 	public String getIsbn() {
 		return isbn;
 	}
-
 
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
 
-
 	public String isReaded() {
 		String leido = "";
-		if(readed == true) {
+		if (readed == true) {
 			leido = "Sí";
-		}else {
+		} else {
 			leido = "No";
 		}
-		
+
 		return leido;
 	}
-
 
 	public void setReaded(boolean readed) {
 		this.readed = readed;
 	}
-	
+
 	public boolean getIsReaded() {
 		return readed;
 	}
-
 
 	public int getTimeReaded() {
 		return timeReaded;
 	}
 
-
 	public void setTimeReaded(int timeReaded) {
 		this.timeReaded = timeReaded;
 	}
-	
+
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		String detailBook = "\n :: BOOK ::" + 
-							"\n Title: " + getTitle() +
-							"\n Editorial: " + getEditorial() + 
-							"\n Edition Date: " + getEdititionDate() +
-							"\n Authors: ";
+		String detailBook = "\n :: BOOK ::" + "\n Title: " + getTitle() + "\n Editorial: " + getEditorial()
+				+ "\n Edition Date: " + getEdititionDate() + "\n Authors: ";
 		for (int i = 0; i < getAuthors().length; i++) {
 			detailBook += "\t" + getAuthors()[i] + " ";
 		}
-		return  detailBook;
+		return detailBook;
 	}
-
 
 	@Override
 	public Date startToSee(Date dateI) {
@@ -83,76 +77,124 @@ public class Book extends Publication implements IVisualizable {
 		return dateI;
 	}
 
-
 	@Override
 	public void stopToSee(Date dateI, Date dateF) {
 		// TODO Auto-generated method stub
 		if (dateF.getTime() > dateI.getTime()) {
-			setTimeReaded((int)(dateF.getTime() - dateI.getTime()));
-		}else {
+			setTimeReaded((int) (dateF.getTime() - dateI.getTime()));
+		} else {
 			setTimeReaded(0);
 		}
 	}
-	
+
 	public void view() {
 		setReaded(true);
 		Date dateI = startToSee(new Date());
-		
-		for (int i = 0; i < 100000; i++) {
-			System.out.println("..........");
-		}
-		
-		//Termine de verla
+
+		int i = 0;
+		do {
+			
+			System.out.println("........");
+			System.out.println("Page "+getPages().get(i).getNumber());
+			System.out.println(getPages().get(i).getContent());
+			System.out.println("........");
+			
+			if (i!=0) {
+				System.out.println("1. Regresar Pagina");
+				
+			}
+			System.out.println("2. Siguiente Pagina");
+			System.out.println("0. Cerrar Libro");
+			
+			System.out.println();
+			
+			int response= AmazonUtil.validateUserResponseMenu(0, 2);
+			
+			if (response==2) {
+				i++;
+			}
+			else if (response==1) {
+				i--;
+			}
+			else if (response==0) {
+				break;
+				
+			}
+		} while (i < pages.size());
+
+		// Termine de verla
 		stopToSee(dateI, new Date());
 		System.out.println();
 		System.out.println("Leíste: " + toString());
 		System.out.println("Por: " + getTimeReaded() + " milisegundos");
 	}
-	
+
 	public static ArrayList<Book> makeBookList() {
 		ArrayList<Book> books = new ArrayList();
 		String[] authors = new String[3];
 		for (int i = 0; i < 3; i++) {
-			authors[i] = "author "+i;
-		}
-		for (int i = 1; i <= 5; i++) {
-			books.add(new Book("Book " + i, new Date(), "editorial " + i, authors));
+			authors[i] = "author " + i;
 		}
 		
+		ArrayList<Page> pages=new ArrayList();
+		int pagina=0;
+		for (int i = 0; i < 3; i++) {
+			pagina=+i;
+			pages.add(new Book.Page(pagina, "El contenido de la pagina :"+pagina));
+		}
+		
+		
+		
+		for (int i = 1; i <= 5; i++) {
+			books.add(new Book("Book " + i, new Date(), "editorial " + i, authors,pages));
+		}
+
 		return books;
 	}
-	
-	public static class Page{
+
+	public ArrayList<Page> getPages() {
+		return pages;
+	}
+
+	public void setPages(ArrayList<Page> pages) {
+		this.pages = pages;
+	}
+
+	public static class Page {
 		private int id;
 		private int number;
 		private String content;
+
 		public int getId() {
 			return id;
 		}
+
 		public void setId(int id) {
 			this.id = id;
 		}
+
 		public int getNumber() {
 			return number;
 		}
+
 		public void setNumber(int number) {
 			this.number = number;
 		}
+
 		public String getContent() {
 			return content;
 		}
+
 		public void setContent(String content) {
 			this.content = content;
 		}
+
 		public Page(int number, String content) {
 			super();
 			this.number = number;
 			this.content = content;
 		}
-		
-		
-		
-		
+
 	}
-	
+
 }
